42cbc29ffaee9435d479eb28bcec379bf417fe01
Wene <wene83@gmx.ch>
2013-08-03 13:38:20 +0200

    changed the animation to some nice looking colors

--- WS2812_Test.ino
+++ WS2812_Test.ino
@@ -9,45 +9,142 @@
 
 #include "FastSPI_LED2.h"
 
-#define NUM_LEDS 240
+#define NUM_LEDS 120
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
-	// sanity check delay - allows reprogramming if accidently blowing power w/leds
-   	delay(2000);
+  // sanity check delay - allows reprogramming if accidently blowing power w/leds
+  delay(1000);
 
-   	// For safety (to prevent too high of a power draw), the test case defaults to
-   	// setting brightness to 25% brightness
-   	LEDS.setBrightness(64);
+  // For safety (to prevent too high of a power draw), the test case defaults to
+  // setting brightness to 50% brightness
+  LEDS.setBrightness(127);
 
-   	LEDS.addLeds<WS2811, 13>(leds, NUM_LEDS);
+  LEDS.addLeds<WS2811, 13>(leds, NUM_LEDS);
+  
+  //reset all the LEDs
+  memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB));
+  LEDS.show();
 }
 
 void loop()
 {
-  //Test LED for LED in red
   for(int i = 0; i < NUM_LEDS; i++)
   {
     memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB));
-    leds[i].g = 128;
+    
+    int pos = i;
+    for(int j = 0; j < 10; j++)
+    {
+      if(pos - j >= 0)
+      {
+        leds[pos - j].g = 128 - (j * 10);
+      }
+      else
+      {
+        leds[pos + NUM_LEDS - j].g = 128 - (j * 10);
+      }
+    }
+    
+    pos = pos + (NUM_LEDS / 6);
+    if(pos >= NUM_LEDS)
+    {
+      pos = pos - NUM_LEDS;
+    }
+
+    for(int j = 0; j < 10; j++)
+    {
+      if(pos - j >= 0)
+      {
+        leds[pos - j].r = 128 - (j * 10);
+      }
+      else
+      {
+        leds[pos + NUM_LEDS - j].r = 128 - (j * 10);
+      }
+    }
+  
+    pos = pos + (NUM_LEDS / 6);
+    if(pos >= NUM_LEDS)
+    {
+      pos = pos - NUM_LEDS;
+    }
+
+    for(int j = 0; j < 10; j++)
+    {
+      if(pos - j >= 0)
+      {
+        leds[pos - j].b = 128 - (j * 10);
+      }
+      else
+      {
+        leds[pos + NUM_LEDS - j].b = 128 - (j * 10);
+      }
+    }
+    
+    pos = pos + (NUM_LEDS / 6);
+    if(pos >= NUM_LEDS)
+    {
+      pos = pos - NUM_LEDS;
+    }
+
+    for(int j = 0; j < 10; j++)
+    {
+      if(pos - j >= 0)
+      {
+        leds[pos - j].g = 128 - (j * 10);
+        leds[pos - j].r = 128 - (j * 10);
+      }
+      else
+      {
+        leds[pos + NUM_LEDS - j].g = 128 - (j * 10);
+        leds[pos + NUM_LEDS - j].r = 128 - (j * 10);
+      }
+    }
+
+    pos = pos + (NUM_LEDS / 6);
+    if(pos >= NUM_LEDS)
+    {
+      pos = pos - NUM_LEDS;
+    }
+    
+    for(int j = 0; j < 10; j++)
+    {
+      if(pos - j >= 0)
+      {
+        leds[pos - j].r = 128 - (j * 10);
+        leds[pos - j].b = 128 - (j * 10);
+      }
+      else
+      {
+        leds[pos + NUM_LEDS - j].r = 128 - (j * 10);
+        leds[pos + NUM_LEDS - j].b = 128 - (j * 10);
+      }
+    }
+
+    pos = pos + (NUM_LEDS / 6);
+    if(pos >= NUM_LEDS)
+    {
+      pos = pos - NUM_LEDS;
+    }
+    
+    for(int j = 0; j < 10; j++)
+    {
+      if(pos - j >= 0)
+      {
+        leds[pos - j].b = 128 - (j * 10);
+        leds[pos - j].g = 128 - (j * 10);
+      }
+      else
+      {
+        leds[pos + NUM_LEDS - j].b = 128 - (j * 10);
+        leds[pos + NUM_LEDS - j].g = 128 - (j * 10);
+      }
+    }
+
+  
     LEDS.show();
-    delay(250);
-  }
-  //Test LED for LED in green
-  for(int i = 0; i < NUM_LEDS; i++)
-  {
-    memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB));
-    leds[i].r = 128;
-    LEDS.show();
-    delay(250);
-  }
-  //Test LED for LED in blue
-  for(int i = 0; i < NUM_LEDS; i++)
-  {
-    memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB));
-    leds[i].b = 128;
-    LEDS.show();
-    delay(250);
+    delay(100);
   }
 }
