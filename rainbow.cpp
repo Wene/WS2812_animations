8ddb3e6cb5cdb8564e65b512aa00d3c65deabfee
Wene <wene83@gmx.ch>
2013-08-20 23:55:04 +0200

    made rainbow code clean and nice

--- rainbow.cpp
+++ rainbow.cpp
@@ -21,101 +21,53 @@ void Rainbow::doNextStep()
 
     memset(pLEDS, 0, iLedCount * sizeof(struct CRGB));
 
-    const int iSector = iLedCount / 6;
-    const int iStepSize = 255 / iSector;
-    const int iMod = iLedCount % 6;
+    const int iSectorSize = iLedCount / 6;
+    int iRest = iLedCount % 6;
 
-    //red to yellow
-    for(int iCurPos = 0; iCurPos < iSector; iCurPos++)
+    for(int iSector = 0; iSector < 6; iSector++)
     {
-        int r = 255;
-        int g = iStepSize * iCurPos;
-        int b = 0;
-        pLEDS[iCurPos] = CRGB(r,g,b);
-    }
-    //yellow to green
-    for(int iCurPos = 0; iCurPos < iSector; iCurPos++)
-    {
-        int r = 255 - iStepSize * iCurPos;
-        int g = 255;
-        int b = 0;
-        pLEDS[iCurPos + iSector] = CRGB(r,g,b);
-    }
-    //green to cyan
-    for(int iCurPos = 0; iCurPos < iSector; iCurPos++)
-    {
-        int r = 0;
-        int g = 255;
-        int b = iStepSize * iCurPos;
-        pLEDS[iCurPos + iSector * 2] = CRGB(r,g,b);
-    }
-    //cyan to blue
-    for(int iCurPos = 0; iCurPos < iSector; iCurPos++)
-    {
-        int r = 0;
-        int g = 255 - iStepSize * iCurPos;
-        int b = 255;
-        pLEDS[iCurPos + iSector * 3] = CRGB(r,g,b);
-    }
-    //blue to magenta
-    for(int iCurPos = 0; iCurPos < iSector; iCurPos++)
-    {
-        int r = iStepSize * iCurPos;
-        int g = 0;
-        int b = 255;
-        pLEDS[iCurPos + iSector * 4] = CRGB(r,g,b);
-    }
-    //magenta to red
-    for(int iCurPos = 0; iCurPos < iSector + iMod; iCurPos++)
-    {
-        int r = 255;
-        int g = 0;
-        int b = 255 - iStepSize * iCurPos;
-        pLEDS[iCurPos + iSector * 5] = CRGB(r,g,b);
-    }
+        int iCurSectorSize = iSectorSize;
+        if(iRest > 0)
+        {
+            iRest--;
+            iCurSectorSize++;
+        }
 
-//    for(int color = 0; color < 6; color++)
-//    {
-//        CRGB curColor;
-//        switch(color)
-//        {
-//        case 0:
-//            curColor = CRGB(255,0,0);
-//            break;
-//        case 1:
-//            curColor = CRGB(0,255,0);
-//            break;
-//        case 2:
-//            curColor = CRGB(0,0,255);
-//            break;
-//        case 3:
-//            curColor = CRGB(200,200,0);
-//            break;
-//        case 4:
-//            curColor = CRGB(200,0,200);
-//            break;
-//        case 5:
-//            curColor = CRGB(0,200,200);
-//            break;
-//        }
+        int iStepSize = 255 / iCurSectorSize;
 
-//        for(int j = 0; j < 10; j++)
-//        {
-//            if(iPos - j >= 0)
-//            {
-//                pLEDS[iPos - j] = curColor;
-//            }
-//            else
-//            {
-//                pLEDS[iPos + iLedCount - j] = curColor;
-//            }
-//            curColor.nscale8(180);
-//        }
+        for(int iCurPos = 0; iCurPos < iCurSectorSize; iCurPos++)
+        {
+            int r = 0, g = 0, b = 0;
+            int iColor = iStepSize * iCurPos;
 
-//        iPos = iPos + (iLedCount / 6);
-//        if(iPos >= iLedCount)
-//        {
-//            iPos = iPos - iLedCount;
-//        }
-//    }
+            switch(iSector)
+            {
+            case 0: //red to yellow
+                r = 255;
+                g = iColor;
+                break;
+            case 1: //yellow to green
+                r = 255 - iColor;
+                g = 255;
+                break;
+            case 2: //green to cyan
+                g = 255;
+                b = iColor;
+                break;
+            case 3: //cyan to blue
+                g = 255 - iColor;
+                b = 255;
+                break;
+            case 4: //blue to magenta
+                r = iColor;
+                b = 255;
+                break;
+            case 5: //magenta to red
+                r = 255;
+                b = 255 - iColor;
+                break;
+            }
+            pLEDS[iCurPos + iSectorSize * iSector] = CRGB(r,g,b);
+        }
+    }
 }
