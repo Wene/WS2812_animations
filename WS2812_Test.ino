f95958695e3e2cfb6daa17c8d86f95a372eb54e0
Wene <wene83@gmx.ch>
2013-09-16 14:48:21 +0200

    Switched SPI pin to 12, removed timer and added button on pin 10 for switching the animations

--- WS2812_Test.ino
+++ WS2812_Test.ino
@@ -1,25 +1,32 @@
 #include "FastSPI_LED2.h"
-#include "colorcircle.h"
-#include "rainbow.h"
 #include "animation.h"
+#include "rainbow.h"        //animation 0
+#include "colorcircle.h"    //animation 1
 
 // define the number of LEDs
-#define NUM_LEDS 100
+#define NUM_LEDS 240
 
 CRGB leds[NUM_LEDS];
 //ColorCircle Animation(leds, NUM_LEDS);
 //Rainbow Animation(leds, NUM_LEDS);
 Animation *Anim;
-int counter = 0;
+bool buttonPressed = false;
+int ledPin = 13;
+int buttonPin = 10;
+int currentAnimation = 0;
 
 void setup() {
-    // sanity check delay - allows reprogramming if accidently blowing power w/leds
+    pinMode(ledPin, OUTPUT);
+    pinMode(buttonPin, INPUT_PULLUP);
+    digitalWrite(ledPin, HIGH);
+
+    //sanity check delay - allows reprogramming if accidently blowing power w/leds
     delay(1000);
 
-    // setting brightness to 12%
-    LEDS.setBrightness(20);
+    //setting maximum brightness
+    LEDS.setBrightness(45);
 
-    LEDS.addLeds<WS2811, 13, GRB>(leds, NUM_LEDS); //GRB for the WS2812 color order
+    LEDS.addLeds<WS2811, 12, GRB>(leds, NUM_LEDS); //GRB for the WS2812 color order
 
     //Rainbow *Ani = new Rainbow(leds, NUM_LEDS);
     Anim = new Rainbow(leds, NUM_LEDS);
@@ -27,22 +34,41 @@ void setup() {
     //reset all the LEDs
     memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB));
     LEDS.show();
+    digitalWrite(ledPin, LOW);
 }
 
 void loop()
 {
-    counter++;
-    if(counter == 600)
+    int buttonState = digitalRead(buttonPin);
+    if(buttonState == LOW)
     {
-        delete Anim;
-        Anim = new ColorCircle(leds, NUM_LEDS);
+        buttonPressed = true;
+        digitalWrite(ledPin, HIGH);
     }
-    if(counter >= 1200)
+    else if(buttonState == HIGH)
     {
-        delete Anim;
-        Anim = new Rainbow(leds, NUM_LEDS);
-        counter = 0;
+        if(buttonPressed)   //switch animation only if the button was pressed before
+        {
+            buttonPressed = false;
+            delete Anim;
+            currentAnimation++;
+            if(currentAnimation > 1)    //reset
+            {
+                currentAnimation = 0;
+            }
+            switch(currentAnimation)
+            {
+            case 0:
+                Anim = new Rainbow(leds, NUM_LEDS);
+                break;
+            case 1:
+                Anim = new ColorCircle(leds, NUM_LEDS);
+                break;
+            }
+        }
+        digitalWrite(ledPin, LOW);
     }
+
     Anim->doNextStep();
     LEDS.show();
     delay(100);
